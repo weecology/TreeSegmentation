4c25d1e9bb2e154561ca9afe12d3ca263b4d7901
bw4sz <benweinstein2010@gmail.com>
2018-06-11 13:59:51 -0700

    nodes can be on diff ranks

--- analysis/SubmitR.sh
+++ analysis/SubmitR.sh
@@ -4,11 +4,10 @@
 #SBATCH --mail-user=ben.weinstein@weecology.org   # Where to send mail
 #SBATCH --account=ewhite
 #SBATCH --qos=ewhite-b
-#SBATCH --nodes=1                 # Number of MPI ranks
 #SBATCH --ntasks=1                 # Number of MPI ranks
 #SBATCH --cpus-per-task=10            # Number of cores per MPI rank
 #SBATCH --mem-per-cpu=20GB
-#SBATCH --time=5:00:00       #Time limit hrs:min:sec
+#SBATCH --time=24:00:00       #Time limit hrs:min:sec
 #SBATCH --output=/home/b.weinstein/logs/training.out   # Standard output and error log
 #SBATCH --error=/home/b.weinstein/logs/training.err
 
