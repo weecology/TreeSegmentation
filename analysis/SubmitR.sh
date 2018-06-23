7214c3f81260543581e7dcf23bd50ea8ba54f05e
bw4sz <benweinstein2010@gmail.com>
2018-06-23 12:59:09 -0700

    training detection

--- analysis/SubmitR.sh
+++ analysis/SubmitR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=crop_rgb   # Job name
+#SBATCH --job-name=detection   # Job name
 #SBATCH --mail-type=END               # Mail events (NONE, BEGIN, END, FAIL, AL$
 #SBATCH --mail-user=ben.weinstein@weecology.org   # Where to send mail
 #SBATCH --account=ewhite
@@ -8,9 +8,9 @@
 #SBATCH --cpus-per-task=10            # Number of cores per MPI rank
 #SBATCH --mem-per-cpu=20GB
 #SBATCH --time=24:00:00       #Time limit hrs:min:sec
-#SBATCH --output=/home/b.weinstein/logs/training.out   # Standard output and error log
-#SBATCH --error=/home/b.weinstein/logs/training.err
+#SBATCH --output=/home/b.weinstein/logs/detection.out   # Standard output and error log
+#SBATCH --error=/home/b.weinstein/logs/detection.err
 
 #This is a generic R submission script
 module load R
-Rscript training.R
+Rscript detection_training.R
