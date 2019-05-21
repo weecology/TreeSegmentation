0616874e903d0495112b666331b60f29aa94e763
bw4sz <benweinstein2010@gmail.com>
2019-05-21 13:15:21 -0700

    batch submission

--- analysis/detection_batchtools.R
+++ analysis/detection_batchtools.R
@@ -34,7 +34,16 @@ if(testing){
   #map each file to a new job
   #debugging
   lidar_files = lidar_files[1:50]
-  ids = batchMap(fun = run_detection,lidar_file=lidar_files,site,rgb_dir=rgb_dir, year=year,silva_cr_factor=site_params$max_cr_factor, silva_exclusion=site_params$exclusion)
+  silva_cr_factor<- site_params$max_cr_factor
+  silva_exclusion<- site_params$exclusion
+
+  ids = batchMap(fun = run_detection,
+                 lidar_file=lidar_files,
+                 site=site,
+                 rgb_dir=rgb_dir,
+                 year=year,
+                 silva_cr_factor=silva_cr_factor,
+                 silva_exclusion=silva_exclusion)
 
   #Run in chunks of 10
   ids[, chunk := chunk(job.id, chunk.size = 10)]
