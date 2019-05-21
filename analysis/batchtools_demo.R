7aa0d1af256d35daf3f2f9df418d89ef078dcbc1
bw4sz <benweinstein2010@gmail.com>
2019-05-21 11:22:16 -0700

    simpler example, right now everything just expires

--- analysis/batchtools_demo.R
+++ analysis/batchtools_demo.R
@@ -7,18 +7,19 @@ print("registry created")
 
 print(reg)
 # Toy function which creates a large matrix and returns the column sums
-fun = function(n, p) colMeans(matrix(runif(n*p), n, p))
-
-# Arguments to fun:
-args = CJ(n = c(1e4, 1e5), p = c(10, 50)) # like expand.grid()
-print(args)
+fun = function(n, p) {
+  Sys.sleep(10)
+  t<-Sys.time()
+  print(t)
+  return(t)
+}
 
 #batchtools submission
 reg$cluster.functions=makeClusterFunctionsSlurm(template = "detection_template.tmpl", array.jobs = TRUE,nodename = "localhost", scheduler.latency = 5, fs.latency = 65)
 ids = batchMap(fun, args = args, reg = reg)
 
 # Set resources: enable memory measurement
-res = list(measure.memory = TRUE,walltime = "2:00:00", memory = "4GB")
+res = list(walltime = "2:00:00", memory = "4GB")
 
 # Submit jobs using the currently configured cluster functions
 submitJobs(ids, resources = res, reg = reg)
