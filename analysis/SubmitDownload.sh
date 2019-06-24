4451d8e2d601b89f9b577b6ebfd25c58f96c38b2
bw4sz <benweinstein2010@gmail.com>
2019-06-24 17:00:44 -0400

    MLBS detection boxes

--- analysis/SubmitDownload.sh
+++ analysis/SubmitDownload.sh
@@ -5,12 +5,12 @@
 #SBATCH --account=ewhite
 #SBATCH --qos=ewhite-b
 #SBATCH --ntasks=1                 # Number of MPI ranks
-#SBATCH --cpus-per-task=10            # Number of cores per MPI rank
+#SBATCH --cpus-per-task=4            # Number of cores per MPI rank
 #SBATCH --mem-per-cpu=5GB
 #SBATCH --time=72:00:00       #Time limit hrs:min:sec
-#SBATCH --output=/home/b.weinstein/logs/download_BART.out   # Standard output and error log
-#SBATCH --error=/home/b.weinstein/logs/download_BART.err
+#SBATCH --output=/home/b.weinstein/logs/download_plots.out   # Standard output and error log
+#SBATCH --error=/home/b.weinstein/logs/download_plots.err
 
 #This is a generic R submission script
 module load gcc/6.3.0 R gdal/2.2.1
-Rscript ParallelDownload.R
+Rscript Process_NEON_Plots.R
