67d03ac1ff86b98c1faee4f40217e1b238b6056a
bw4sz <benweinstein2010@gmail.com>
2019-08-12 13:11:24 -0700

    download loop

--- analysis/ParallelDownload.R
+++ analysis/ParallelDownload.R
@@ -7,14 +7,19 @@ library(neonUtilities)
 #TO DO, make %dopar% available without load
 library(foreach)
 
-site="BART"
+sites=c("HARV","WREF")
 
-fold<-paste("/orange/ewhite/NeonData/",site=site,sep="")
-#RGB
-ParallelFileAOP(dpID = "DP3.30010.001",site = site,year="2018",check.size=F, savepath=fold,cores=5)
+for(site in sites){
+  print(site)
+  fold<-paste("/orange/ewhite/NeonData/",site=site,sep="")
 
-#LIDAR
-ParallelFileAOP(dpID = "DP1.30003.001",site = site,year="2018",check.size=F, savepath=fold,cores=5)
+  #RGB
+  ParallelFileAOP(dpID = "DP3.30010.001",site = site,year="2018",check.size=F, savepath=fold,cores=5)
 
-#Hyperspec
-ParallelFileAOP(dpID = "DP3.30006.001",site = site,year="2018",check.size=F, savepath=fold,cores=5)
+  #LIDAR
+  ParallelFileAOP(dpID = "DP1.30003.001",site = site,year="2018",check.size=F, savepath=fold,cores=5)
+
+  #Hyperspec
+  ParallelFileAOP(dpID = "DP3.30006.001",site = site,year="2018",check.size=F, savepath=fold,cores=5)
+
+}
