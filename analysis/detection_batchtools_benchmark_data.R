f1950a4ee4334cf65f862f65878667c43fe59232
bw4sz <benweinstein2010@gmail.com>
2019-10-21 11:50:51 -0700

    doesn't line up

--- analysis/detection_batchtools_benchmark_data.R
+++ analysis/detection_batchtools_benchmark_data.R
@@ -18,7 +18,7 @@ reg$cluster.functions=makeClusterFunctionsSlurm(template = "detection_template.t
 site_df<-data.frame(Site=c("SJER","TEAK","NIWO","MLBS","BART","BLAN","BONA","CLBJ","DELA","DSNY","HARV","JERC","LENO","ONAQ","OSBS","SCBI","SOAP","TALL","UNDE","WREF"),Type=c("D","C","C","D","D","D","D","D","D","C","D","D","D","C","C","D","D","D","D","C"))
 parameter_df<-data.frame(Type=c("C","D"),max_cr_factor=c(0.2,0.9),exclusion=c(0.4,0.3))
 site_df<-merge(site_df,parameter_df)
-tiles<-list.files("/orange/ewhite/b.weinstein/NeonTreeEvaluation",pattern=".laz",full.names = T)
+tiles<-list.files("/orange/ewhite/b.weinstein/NeonTreeEvaluation/pretraining/",pattern=".laz",full.names = T)
 sites<-str_match(tiles,"NEON_\\w+_(\\w+)_DP1")[,2]
 tile_df<-data.frame(Site=sites,Tile=tiles)
 batch_df<-merge(site_df,tile_df)
