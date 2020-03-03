b6303cd8a6a08f5a58a2cf3bdf69ee1a77e1c8a3
bw4sz <benweinstein2010@gmail.com>
2020-03-03 10:03:42 -0800

    working on reticulate

--- analysis/crop_field_extent.R
+++ analysis/crop_field_extent.R
@@ -1,7 +1,7 @@
 #Find crops
 library(raster)
 library(TreeSegmentation)
-rgb_filename<-"/home/b.weinstein/TreeSegmentation/data/field_crowns/1.tif"
+rgb_filename<-"/home/b.weinstein/TreeSegmentation/data/field_crowns/2.tif"
 basename <- stringr::str_match(rgb_filename,"/(\\w+).tif")[,2]
 
 #CROP CHM
@@ -13,4 +13,10 @@ easting <- as.integer(ext@xmin/1000)*1000
 northing <- as.integer(ext@ymin/1000)*1000
 geo_index <- paste(easting,northing,sep="_")
 
-crop_target_hyperspectral(siteID="MLBS",rgb_filename,geo_index,false_color=TRUE, year="2019",h5_base_dir="/orange/ewhite/NeonData",save_base_dir="/orange/ewhite/b.weinstein/NEON")
+crop_target_hyperspectral(siteID="MLBS",
+                          rgb_filename,
+                          geo_index,
+                          false_color=TRUE,
+                          year="2019",
+                          h5_base_dir="/orange/ewhite/NeonData/",
+                          save_base_dir="/orange/ewhite/b.weinstein/NEON/")
--- analysis/generate_h5_raster.py
+++ analysis/generate_h5_raster.py
@@ -10,10 +10,3 @@ def run(h5_path,rgb_filename,save_dir,false_color=True):
         print("{} : {}".format(rgb_filename,status))
     except Exception as e:
         print("{} : {}".format(rgb_filename,e))
-    
-#run(h5_path="/orange/ewhite/NeonData/SCBI/DP3.30006.001/2019/FullSite/D02/2019_SCBI_3/L3/Spectrometer/Reflectance/NEON_D02_SCBI_DP3_747000_4309000_reflectance.h5",
-    #rgb_filename="/orange/ewhite/b.weinstein/NEON/SCBI/2019/NEONPlots/Camera/L3/SCBI_002.tif",
-    #save_dir="/orange/ewhite/b.weinstein/NEON/SCBI/2019/NEONPlots/Hyperspectral/L3/",
-    #false_color=False)
-
-    
