16c08bdb23a34716aedf6875cdec04f5adf7555c
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-02-18 10:44:19 -0800

    don't allow __main__ args in reticulate

--- analysis/generate_h5_raster.py
+++ analysis/generate_h5_raster.py
@@ -15,30 +15,10 @@ def parse_args():
     
     return(parser.parse_args())
     
-def run(h5_path,rgb_filename,save_dir,false_color=True):
+def run(h5_path,rgb_filename,save_dir,false_color=False):
     #Crop and create raster
     try:
         status = Hyperspectral.generate_raster(h5_path = h5_path, rgb_filename=rgb_filename, false_color=false_color, save_dir=save_dir)
         print("{} : {}".format(rgb_filename,status))
     except Exception as e:
         print("{} : {}".format(rgb_filename,e))
-        
-if __name__ == "__main__":
-    
-    #parse args
-    args = parse_args()
-    
-    #get geoindex
-    #crop Hyperspectral 3 band
-    src = rasterio.open(args.rgb_filename)
-    ext = src.bounds
-    
-    #get geodata
-    easting = int(ext.left/1000)*1000
-    northing = int(ext.bottom/1000)*1000
-    geo_index = "{}_{}".format(easting,northing)
-    
-    h5_files=glob.glob("/orange/ewhite/NeonData/{}/DP3.30006.001/{}/**/Reflectance/*.h5".format(args.siteID,args.year),recursive=True)    
-    h5_path = [x for x in h5_files if geo_index in x][0]
-    
-    Hyperspectral.generate_raster(h5_path = h5_path, rgb_filename=args.rgb_filename, false_color=args.false_color, save_dir=args.save_dir)
